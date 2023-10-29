7fecc1b194b93662acb353995066625ae23a3f68
doesitworkornot <chernov.ivan.al@gmail.com>
2023-10-29 16:08:52 +0700

    lab 8

---
+++ 8.1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv('data/lab8/wells_info_with_prod.csv')
+X = pd.DataFrame(columns=['water', 'len'])
+y = df.Prod1Year
+X.water = df.WATER_PER_FOOT
+X.len = df.LATERAL_LENGTH_BLEND
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+scaler = StandardScaler()
+features_train_scaled = scaler.fit_transform(X_train)
+target_var_scaled = scaler.fit_transform(y_train.values.reshape(-1, 1))
+
+
+features_test_scaled = scaler.transform(X_test.values.reshape(-1, 1))
+target_test_scaled = scaler.transform(y_test.values.reshape(-1, 1))
